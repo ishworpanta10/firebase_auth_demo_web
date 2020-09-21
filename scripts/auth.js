1459e1f479073692fd3ce04469cd37e78edef56b
ishworpanta10 <ishworpanta10@gmail.com>
2020-09-21 21:01:22 +0545

    SignIn, SignOutand logIn  user to firebase completed

---
+++ scripts/auth.js
@@ -0,0 +1,46 @@
+
+// Signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+    // default behavuor is reloading so
+    e.preventDefault();
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    //signed up user
+    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+        // console.log(cred);
+        const modal = document.querySelector('#modal-signup');
+        M.Modal.getInstance(modal).close();
+        signupForm.reset();
+    });
+});
+
+// logout
+const logOut = document.querySelector('#logout');
+logOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        // console.log("user logged out");
+    })
+});
+
+// login 
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //get user info
+    const email = signupForm['login-email'].value;
+    const password = signupForm['login-password'].value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        // console.log(cred.user)
+        // close the login modal and reset the login form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close();
+        loginForm.reset();
+    });
+});
+
+
+
